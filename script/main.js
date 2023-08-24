fc197e462f93f57842e119eec20ebd4408134b59
dev-Pipo <141969120+dev-Pipo@users.noreply.github.com>
2023-08-24 18:30:41 -0300

    Update prestamos main.js

--- script/main.js
+++ script/main.js
@@ -13,3 +13,38 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+
+const cacl = document.querySelector(".calcular")
+
+function calcularPorcentaje (a, b){
+    var interes;
+        if(a <= 3)i = 0.025; // 2.5%
+        else if(a > 3 && a < 6) interes = 0.05; // 5% 
+        else if(a > 6 && a < 10) interes = 0.075 // 7.5% 
+        else if (a > 10 && a < 14) interes = 0.1 // 10%
+
+    let operacion =  b * a * interes;
+    return operacion
+    }
+
+function calcularTotal(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return number1 + number2
+} 
+
+cacl.addEventListener("click",()=>{
+    const monto = document.querySelector(".amount").value;
+    const meses = document.querySelector(".month").value;
+    const tax = document.querySelector(".tax")
+    const amountResult = document.querySelector(".amount-result")
+
+    let taxResult = calcularPorcentaje(meses, monto); 
+    let final = calcularTotal(monto, taxResult);
+    amountResult.textContent = `Monto a devolver: $${final}`
+    tax.textContent =`intereses: $${taxResult}`;
+    
+
+}
+)
